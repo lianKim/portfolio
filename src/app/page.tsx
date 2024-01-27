df540baabf28df671cfd460c2d31ff99c4df9da6
Lian Kim <5ffcut@gmail.com>
2024-01-27 18:37:58 +0900

    Feat : R3F로 3D 명함 컴포넌트 개발

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import About from '@/components/about/About'
+import BusinessCardAnimation from '@/components/landing/BusinessCardAnimation'
 import Work from '@/components/work/Work'
 
 export default function Home() {
   return (
     <main className="main">
+      <BusinessCardAnimation />
       <About />
       <Work />
     </main>
