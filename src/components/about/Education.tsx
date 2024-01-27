7de86c57fc4c267093476d87679aea6dced1ba56
Lian Kim <5ffcut@gmail.com>
2024-01-27 16:27:34 +0900

    Feat : Education 타임라인 차트 개발

--- src/components/about/Education.tsx
+++ src/components/about/Education.tsx
@@ -2,7 +2,7 @@
 import React, { useMemo } from 'react'
 import styles from '@/styles/About.module.css'
 import { EducationItemData } from '@/types/education'
-import { getPeriodOfWork } from '@/lib/utils/handleString'
+import EducationTimeline from './EducationTimeline'
 
 interface EducationProps {
   data: EducationItemData[]
@@ -12,12 +12,14 @@ export default function Education({ data }: EducationProps) {
   const educationList = useMemo(
     () =>
       data.map((item: EducationItemData) => {
-        const title = item.properties?.Name.title?.at(0)?.plain_text
+        const title = item.properties?.Name.title?.at(0)?.plain_text || ''
         const startDate = item.properties?.Period.date?.start || ''
-        const endDate = item.properties?.Period.date?.end
-        const period = getPeriodOfWork(startDate, endDate)
+        const endDate = item.properties?.Period.date?.end || ''
 
-        return { title, period }
+        return {
+          x: title,
+          y: [new Date(startDate).getTime(), new Date(endDate).getTime()],
+        }
       }),
     [data],
   )
@@ -25,18 +27,7 @@ export default function Education({ data }: EducationProps) {
   return (
     <div className={styles['grid-container']}>
       <h3 className="sub-title">Education</h3>
-      <ol className={`${styles.content} ${styles.education}`}>
-        {educationList.map((education) => (
-          <li className={styles['education-item']} key={education.period}>
-            <span className={`${styles['education-title']} font-kor`}>
-              {education.title}
-            </span>
-            <span className={styles['education-duration']}>
-              {education.period}
-            </span>
-          </li>
-        ))}
-      </ol>
+      <EducationTimeline dataList={educationList} />
     </div>
   )
 }
