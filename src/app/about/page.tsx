954d5509ccdda90b5685ab037ca0e41d98cac933
Lian Kim <5ffcut@gmail.com>
2024-02-13 20:56:11 +0900

    Fix : about 페이지 window is not defined 에러 해결

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
-import About from '@/components/about/About'
+import dynamic from 'next/dynamic'
 import React from 'react'
+
+const About = dynamic(async () => await import('@/components/about/About'), {
+  ssr: false,
+})
 
 export default function AboutPage() {
   return (
