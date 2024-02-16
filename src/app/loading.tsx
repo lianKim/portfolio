67a8291a026bc8684c503730f9b6b7be6dd86061
Lian Kim <5ffcut@gmail.com>
2024-02-16 18:39:43 +0900

    Design : 헤더 nav 애니메이션 구현 및 로딩 컴포넌트 임시 디자인 적용

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,7 @@
+'use client'
+import IntersectionObserverContainer from '@/components/@common/IntersectionObserverContainer'
 import React from 'react'
 
 export default function Loading() {
-  return <div className="loading">Loading...</div>
+  return <div className="loading">loading...</div>
 }
