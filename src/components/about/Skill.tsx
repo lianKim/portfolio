fd95a4ad232df3470a18c4ccfdf8d74897f047db
Lian Kim <5ffcut@gmail.com>
2024-02-17 17:16:01 +0900

    Refactor : skill 이미지 요소 삭제

--- src/components/about/Skill.tsx
+++ src/components/about/Skill.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useMemo, useState } from 'react'
 import styles from '@/styles/About.module.css'
-import Image from 'next/image'
 import { SkillItemData } from '@/types/skill'
 import SectionTitle from '../@common/SectionTitle'
 
@@ -9,21 +8,14 @@ interface SkillProps {
   data: SkillItemData[]
 }
 
-interface SkillItemProps {
-  name?: string
-  imgSrc?: string
-}
-
 export default function Skill({ data }: SkillProps) {
   const skillList = useMemo(
     () =>
       data
-        .map((item: SkillItemData) => {
-          const title = item.properties?.Name.title?.at(0)?.plain_text
-          const imageUrl = item.properties?.Image.files?.at(0)?.file?.url
-
-          return { title, imageUrl }
-        })
+        .map(
+          (item: SkillItemData) =>
+            item.properties?.Name.title?.at(0)?.plain_text,
+        )
         .reverse(),
     [data],
   )
@@ -33,51 +25,11 @@ export default function Skill({ data }: SkillProps) {
       <SectionTitle title="Skill" />
       <ul className={`${styles.content} ${styles.skill}`}>
         {skillList.map((skill) => (
-          <SkillItem
-            key={skill.title}
-            name={skill.title}
-            imgSrc={skill.imageUrl}
-          />
+          <li className={`${styles['skill-item']}`} key={skill}>
+            <h4>{skill}</h4>
+          </li>
         ))}
       </ul>
     </section>
-  )
-}
-
-function SkillItem({ name, imgSrc }: SkillItemProps) {
-  const [hovered, setHovered] = useState(false)
-
-  const handleMouseOver = () => {
-    setHovered(true)
-  }
-
-  const handleMouseOut = () => {
-    setHovered(false)
-  }
-
-  if (!name) return
-
-  return (
-    <li
-      className={`${styles['skill-item']}`}
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      onClick={handleMouseOut}
-    >
-      <h4>{name}</h4>
-      {hovered && imgSrc && (
-        <div className={styles['skill-logo-container']}>
-          <Image
-            className={styles['skill-logo']}
-            src={imgSrc}
-            alt={`${name} icon`}
-            width={100}
-            height={100}
-            style={{ width: '100%', height: '100%' }}
-            loading="lazy"
-          />
-        </div>
-      )}
-    </li>
   )
 }
