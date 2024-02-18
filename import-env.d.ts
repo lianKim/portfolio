c1bc6fe7d6053321472d8f36ecc44ac26ac49ec1
Lian Kim <5ffcut@gmail.com>
2024-02-18 14:17:11 +0900

    Refactor : 미사용 환경변수 및 API와 Skill 섹션 로고 이미지 파일 삭제

--- import-env.d.ts
+++ import-env.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_NOTION_API_KEY: string
-    NEXT_PUBLIC_CONTACT_DB_ID: string
     NEXT_PUBLIC_SKILL_DB_ID: string
     NEXT_PUBLIC_EDUCATION_DB_ID: string
     NEXT_PUBLIC_WORK_DB_ID: string
