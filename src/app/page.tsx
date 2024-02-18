c0d7401df2ff11d861b4c2d2ae3640c6e5957da9
Lian Kim <5ffcut@gmail.com>
2024-02-18 12:55:14 +0900

    Refactor : R3F 명함 컴포넌트 dynamic import 옵션 loading 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import Loading from './loading'
 
 const About = dynamic(async () => await import('@/components/about/About'), {
   ssr: false,
@@ -10,7 +11,7 @@ const Work = dynamic(async () => await import('@/components/work/Work'), {
 
 const BusinessCardAnimation = dynamic(
   async () => await import('@/components/landing/BusinessCardAnimation'),
-  { ssr: false },
+  { ssr: false, loading: () => <Loading /> },
 )
 
 export default function Home() {
