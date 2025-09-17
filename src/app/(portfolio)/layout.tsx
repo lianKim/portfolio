c46c0938dd7e57c34c496694d64a43e6657b95bc
Lian Kim <5ffcut@gmail.com>
2025-09-17 12:28:39 +0900

    fix: Route Group으로 기존 포트폴리오 경로 분리

---
+++ src/app/(portfolio)/layout.tsx
@@ -0,0 +1,36 @@
+import './globals.css'
+
+import Header from '@/components/@common/Header'
+import LinkedIcons from '@/components/@common/LinkedIcons'
+import type { Metadata } from 'next'
+
+interface RootLayoutProps {
+  children: React.ReactNode
+  modal: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: 'Lian Kim | Portfolio',
+  description: '프론트엔드 개발자 김리안의 포트폴리오입니다.',
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
+
+export default function RootLayout({ children, modal }: RootLayoutProps) {
+  return (
+    <html lang="ko">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/uhv3fvd.css" />
+      </head>
+      <body>
+        <div className="grid-container">
+          <Header />
+          <LinkedIcons />
+          <main className="main">{children}</main>
+          {modal}
+        </div>
+      </body>
+    </html>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,11 @@
-import type { Metadata } from 'next'
-import '@/styles/globals.css'
-import './globals.css'
-import Header from '@/components/@common/Header'
-import LinkedIcons from '@/components/@common/LinkedIcons'
-
-interface RootLayoutProps {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode
-  modal: React.ReactNode
-}
-
-export const metadata: Metadata = {
-  title: 'Lian Kim | Portfolio',
-  description: '프론트엔드 개발자 김리안의 포트폴리오입니다.',
-  icons: {
-    icon: '/favicon.ico',
-  },
-}
-
-export default function RootLayout({ children, modal }: RootLayoutProps) {
+}) {
   return (
-    <html lang="ko">
-      <head>
-        <link rel="stylesheet" href="https://use.typekit.net/uhv3fvd.css" />
-      </head>
-      <body>
-        <div className="grid-container">
-          <Header />
-          <LinkedIcons />
-          <main className="main">{children}</main>
-          {modal}
-        </div>
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   )
 }
